5ae7616c490f36c04fbd69f27f26738d0406f084
APAC\PGS2KOR <souravkumar.panghal@in.bosch.com>
2023-01-09 10:57:44 +0530

    intial commit with code changes

---
+++ limits.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  9 10:33:56 2023
+
+@author: PGS2KOR
+"""
+LIMITS = {
+    'PASSIVE_COOLING' : (0,35),
+    'HI_ACTIVE_COOLING' : (0,45),
+    'MED_ACTIVE_COOLING' : (0,40),
+    }
---
+++ send_alerts.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  9 10:40:50 2023
+
+@author: PGS2KOR
+"""
+def send_to_controller(breachType):
+  header = 0xfeed
+  print(f'{header}, {breachType}')
+
+
+def send_to_email(breachType):
+  recepient = "a.b@c.com"
+  if breachType == 'TOO_LOW':
+    print(f'To: {recepient}')
+    print('Hi, the temperature is too low')
+  elif breachType == 'TOO_HIGH':
+    print(f'To: {recepient}')
+    print('Hi, the temperature is too high')
