a0559c91e4f704d8239240296d43c7414ed64219
APAC\PGS2KOR <souravkumar.panghal@in.bosch.com>
2023-01-09 11:00:28 +0530

    entering reflections

--- typewise_alert.py
+++ typewise_alert.py
@@ -14,7 +14,7 @@ def infer_breach(value, lowerLimit, upperLimit):
 def check_and_alert(alertTarget, batteryChar, temperatureInC):
   lowerlimit, upperlimit = LIMITS(batteryChar['coolingType'])
   breachType =\
-    infer_breach(temperatureInC, lowerLimit, upperLimit)
+    infer_breach(temperatureInC, lowerlimit, upperlimit)
   if alertTarget == 'TO_CONTROLLER':
     send_to_controller(breachType)
   elif alertTarget == 'TO_EMAIL':
